7a9e64304be2816801c38079d18269bee5d8b3d4
Ganning Xu <ganningxu@gmail.com>
2021-11-17 23:37:40 -0500

    avl_height()

--- avl_tree.c
+++ avl_tree.c
@@ -14,10 +14,18 @@ void avl_insert(NodePtr *root, int data);
 void avl_delete(NodePtr *root, int data);
 void avl_print(NodePtr root);
 void avl_print_inorder(NodePtr root);
+int avl_height(NodePtr root);
 
 int main(void)
 {
     NodePtr root = NULL;
 
     return 0;
+}
+
+int avl_height(NodePtr root)
+{
+    if (root == NULL)
+        return -1;
+    return root->height;
 }
