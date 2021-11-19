6b8e4149bbec9cc63d0a53e4fc9445f05e22a083
Ganning Xu <ganningxu@gmail.com>
2021-11-19 10:11:19 -0500

    no balancing yet

--- avl_tree.c
+++ avl_tree.c
@@ -21,6 +21,7 @@ size_t height(NodePtr node);
 void leftRotate(NodePtr *bst);
 void rightRotate(NodePtr *bst);
 int getBalance(NodePtr node);
+NodePtr *successor_bst(NodePtr *treePtr);
 
 int main(void)
 {
@@ -35,9 +36,15 @@ int main(void)
     avl_insert(&root, 8);
 
     print_bst(root);
+
+    puts("---------");
+    avl_delete(&root, 7);
+
+    print_bst(root);
     return 0;
 }
 
+// important functions
 void avl_insert(NodePtr *root, int data)
 {
     NodePtr bst = *root;
@@ -60,7 +67,6 @@ void avl_insert(NodePtr *root, int data)
     }
 
     bst = *root;
-    puts("here1");
     // code below runs after we have inserted a node
     bst->height = 1 + max(height(bst->left), height(bst->right));
     int balance = getBalance(bst);
@@ -84,6 +90,17 @@ void avl_insert(NodePtr *root, int data)
     }
 
     *root = bst;
+}
+
+NodePtr *successor_bst(NodePtr *treePtr)
+{
+    //
+    NodePtr tree = *treePtr;
+    if (tree->left == NULL)
+    {
+        return treePtr;
+    }
+    return successor_bst(&(tree->left));
 }
 
 void leftRotate(NodePtr *bst)
@@ -111,21 +128,6 @@ void rightRotate(NodePtr *bst)
     bst_left->height = max(height(bst_left->left), height(bst_left->right)) + 1;
     *bst = bst_left;
 }
-
-int getBalance(NodePtr node)
-{
-    if (!node)
-        return 0;
-    return height(node->left) - height(node->right);
-}
-
-size_t height(NodePtr node)
-{
-    if (!node)
-        return 0;
-    return node->height;
-}
-
 void print_bst(NodePtr bst)
 {
     /*
@@ -149,6 +151,41 @@ void print_bst(NodePtr bst)
     --depth;
 }
 
+void avl_delete(NodePtr *root, int data)
+{
+    NodePtr bst = *root;
+    if (!bst) // if the bst is empty, we can't delete anything
+        return;
+
+    if (bst->data > data)
+        avl_delete(&(bst->left), data);
+    else if (bst->data < data)
+        avl_delete(&(bst->right), data);
+    else
+    {
+        NodePtr newPtr;
+        if (bst->left && bst->right)
+        {
+            NodePtr *successorPtr = successor_bst(&(bst->right));
+            newPtr = *successorPtr;        // deref to get successor
+            *successorPtr = newPtr->right; // promote the right child to where the deleted node is
+
+            // copy original nodes childrent to successor
+            newPtr->right = bst->right;
+            newPtr->left = bst->left;
+        }
+        else
+        {
+            // no children or one child
+            newPtr = bst->left != NULL ? bst->left : bst->right;
+        }
+
+        *root = newPtr;
+        free(bst);
+    }
+}
+
+// helper functions
 int avl_height(NodePtr root)
 {
     if (root == NULL)
@@ -158,5 +195,22 @@ int avl_height(NodePtr root)
 
 int max(int a, int b)
 {
+    // returns the max value of two numbers
     return (a > b) ? a : b;
+}
+
+int getBalance(NodePtr node)
+{
+    // get the balance factor of the left and right subtrees
+    if (!node)
+        return 0;
+    return height(node->left) - height(node->right);
+}
+
+size_t height(NodePtr node)
+{
+    // returns the height of a node
+    if (!node)
+        return 0;
+    return node->height;
 }
